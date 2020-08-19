ba9dd170bf8a5f2a671c3a9e392e78d00ae61ddf
mauriayllon <mauricio.ad1707@gmail.com>
2020-08-19 15:36:17 -0400

    examen 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,29 @@ import { Component, VERSION } from '@angular/core';
   styleUrls: [ './app.component.css' ]
 })
 export class AppComponent  {
+
+  platos=[{
+nombre : 'khao soi', stock:1, precio : 20, tipo : 'internacional'
+},{
+nombre : 'sajta' , stock: 2, precio : 42, tipo : 'nacional',existe: true
+},{
+nombre : 'semla', stock: 0, precio : 52, tipo : 'internacional',existe: true
+},{
+nombre : 'silpancho', stock: 45, precio : 5, tipo : 'nacional',existe: true
+},{
+nombre : 'pulao', stock: 20, precio : 41, tipo : 'internacional',existe: true
+},{
+nombre : 'saice', stock: 1, precio : 7, tipo : 'nacional',existe: true
+},{
+nombre : 'poutine', stock: 4, precio : 10, tipo : 'internacional',existe: true
+},{
+nombre : 'chicharron', stock: 0, precio : 4, tipo : 'nacional',existe: true
+},
+{nombre : 'fricase', stock: 33, precio : 47, tipo :'nacional',existe: true
+},
+{
+nombre : 'sushi', stock: 75, precio : 5, tipo : 'internacional',existe: true}
+
+]
   name = 'Angular ' + VERSION.major;
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,11 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+import { ExamenComponent } from './components/examen/examen.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
+  declarations: [ AppComponent, HelloComponent, ExamenComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
