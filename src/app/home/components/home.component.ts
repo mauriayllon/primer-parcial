3b967f8a2e7143f7ce39a01b5cc5771595a0f4e5
mauriayllon <mauricio.ad1707@gmail.com>
2020-09-04 17:38:19 -0400

    back up

--- src/app/home/components/home.component.ts
+++ src/app/home/components/home.component.ts
@@ -1,39 +1,89 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,OnDestroy } from '@angular/core';
 import { Subscription } from 'rxjs';
 import { TicketService } from '../../services/ticket.service';
-import { FormBuilder } from '@angular/forms';
-
-
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+ 
+ 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit, OnDestroy {
-
+ 
   products=[];
-
+ 
+  productForm : FormGroup;
+  //nameControl = new FormControl();
+ 
   productSubs: Subscription;
   productGetSubs: Subscription;
   productDeleteSubs: Subscription;
   productUpdateSubs: Subscription;
-  
-  constructor(private formBuilder: FormBuilder,private productService: TicketService) { }
-
-  ngOnInit(): void {
-    this.productSubs = this.productService.getProducts().subscribe(res => {
-      Object.entries(res).map(p => this.products.push(p[1]));
+  idEdit: any;
+ 
+  constructor(private formBuilder: FormBuilder, private productService: TicketService) {}
+ 
+  ngOnInit() {
+    this.loadProduct();
+    this.productForm = this.formBuilder.group({
+      enable: true,
+      name:['', [Validators.minLength(3)]],
+      stock:'',
+      typee:['',[Validators.required]],
+      urlImage:''
+    })
+  } 
+ 
+  loadProduct(): void {
+    this.products = [];
+    this.productGetSubs = this.productService.getProducts().subscribe(res => {
+      Object.entries(res).map((p: any) => this.products.push({id: p[0], ...p[1]}));
     });
   }
+ 
+  onDelete(id: any): void {
+   this.productDeleteSubs = this.productService.deleteProduct(id).subscribe(
+      res => {
+        console.log('RESPONSE: ', res);
+        this.loadProduct();
+      },
+      err => {
+        console.log('ERROR: ');
+      }
+    );
+  }
+ 
+  onEdit(product): void {
+    this.idEdit = product.id;
+    this.productForm.patchValue(product);
+  }
+ 
+  onUpdateProduct(): void {
+    this.productUpdateSubs = this.productService.updateProduct(this.idEdit, this.productForm.value).subscribe(
+      res => {
+        console.log('RESP UPDATE: ', res);
+        this.loadProduct();
+      },
+      err => {
+        console.log('ERROR UPDATE DE SERVIDOR');
+      }
+    );
+  }
+ 
+  onEnviar2(){
+    console.log('Form group: ',this.productForm.value);
+    this.productSubs = this.productService.addProduct(this.productForm.value).subscribe(
+      res => {console.log('Resp: ', res)}, err =>{
+        console.log('Error de servidor')
+      })
+  }
+ 
   ngOnDestroy(){
-    this.productSubs ? this.productSubs.unsubscribe():'';
-  }
-
-  comprar(nombre:string){
-  let index = this.products.findIndex(s=>s.name == nombre);
-  this.products[index].stock = this.products[index].stock-1;
-  if(this.products[index].stock==0){
-    this.products[index].enable==false;
-  }
-}
+    this.productSubs ? this.productSubs.unsubscribe():''; 
+    this.productGetSubs ? this.productGetSubs.unsubscribe():'';
+    this.productDeleteSubs ? this.productDeleteSubs.unsubscribe():'';
+    this.productUpdateSubs ? this.productUpdateSubs.unsubscribe():'';
+    }
+ 
 }
