623de9f5324d50869497a7d6052870d8ce0e45a7
mauriayllon <mauricio.ad1707@gmail.com>
2020-09-17 14:31:16 -0400

    tercer-parcial ready

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,40 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
-
+import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
-import { ExamenComponent } from './components/examen/examen.component';
+import { FormsModule } from '@angular/forms';
+import { NgbModule } from "@ng-bootstrap/ng-bootstrap";
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatSliderModule} from 
+'@angular/material/slider';
+import {MatIconModule} from '@angular/material/icon';
+import { HttpClientModule, } from '@angular/common/http';
+import {StoreModule} from '@ngrx/store';
+import {EffectsModule} from '@ngrx/effects';
+import {StoreDevtoolsModule} from '@ngrx/store-devtools';
+import {environment} from '../environments/environment';
+import {metaReducers} from './core/meta';
+import {reducers} from './core'
+
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent, ExamenComponent ],
-  bootstrap:    [ AppComponent ]
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    NgbModule,
+    MatSliderModule,
+    MatIconModule,
+    HttpClientModule,
+    StoreModule.forRoot(reducers, {metaReducers}),
+    EffectsModule.forRoot([]),
+    StoreDevtoolsModule.instrument({name: 'Angular Course', logOnly: environment.production})
+  ], 
+  exports: [],
+  bootstrap: [AppComponent],
+  providers: [
+  ]
 })
-export class AppModule { }
+export class AppModule { } 
